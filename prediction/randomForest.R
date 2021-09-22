6b1d65e57cc8d42a4a246470d08ae13546f48161
Aman Sinha <aksinha.101010@gmail.com>
2021-09-22 16:37:43 +0000

    use helper functions

--- prediction/randomForest.R
+++ prediction/randomForest.R
@@ -1,31 +1,14 @@
 library(randomForest)
+library(e1071)
+source("utils/helpers.R")
+
 
 # Load dataset
 data <- read.csv(file = "dataset/dataset.csv", nrows = 10000)
 
-# Remove unnecessary field
-data$prefix = NULL
-data$id = NULL
-data$spkid = NULL
-data$full_name = NULL
-data$pdes = NULL
-data$name = NULL
-data$orbit_id = NULL
-data$equinox = NULL
+data <- dataCleaning(data)
 
-# Remove NA data
-data <- na.omit(data)
-
-# Structure
-str(data)
-
-# Categorical data
-data <- transform(
-  data,
-  neo = as.factor(neo),
-  pha = as.factor(pha),
-  class = as.factor(class)
-)
+data <- featureSelection(data)
 
 
 # Split dataset
@@ -33,15 +16,11 @@ sample = sample.split(data$pha, SplitRatio = 0.75)
 train = subset(data, sample == TRUE)
 test = subset(data, sample == FALSE)
 
-dim(train)
-dim(test)
 
 # Create model
 model <- randomForest(pha ~ ., data = train)
-
 model
 
-# predict test set
+# Predict test set
 pred = predict(model, test)
-
-table(test$pha, pred)
+confusionMatrix(table(test$pha, pred))
