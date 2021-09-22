66963e8d0e1092e21006a3f1f5edcf81181635e5
Aman Sinha <aksinha.101010@gmail.com>
2021-09-22 16:36:48 +0000

    add naive bayes classification

---
+++ prediction/naiveBayes.R
@@ -0,0 +1,24 @@
+library(naivebayes)
+library(e1071)
+
+source("utils/helpers.R")
+
+# Load dataset
+data <- read.csv(file = "dataset/dataset.csv", nrows = 10000)
+
+data <- dataCleaning(data)
+
+data <- featureSelection(data)
+
+# Split dataset
+sample = sample.split(data$pha, SplitRatio = 0.75)
+train = subset(data, sample == TRUE)
+test = subset(data, sample == FALSE)
+
+# Create model
+model <- naive_bayes(pha ~ ., data = train)
+model
+
+# Predict test set
+pred <- predict(model, test)
+confusionMatrix(table(test$pha, pred))
