685ab868c48ee1d144034d7b717adf9c98a958ff
Aman Sinha <aksinha.101010@gmail.com>
2021-09-22 16:35:58 +0000

    utils for data cleaning and feature selection

---
+++ utils/helpers.R
@@ -0,0 +1,31 @@
+featureSelection <- function(data) {
+  # Highly correlated features
+  correlationMatrix <- cor(data[, names(data) != "pha"])
+  
+  # Index of highly correlation features
+  hightlyCorrelated <- findCorrelation(correlationMatrix,
+                                       cutoff = 0.9, names = TRUE)
+  
+  # Remove highly correlated features
+  data <- data[, !names(data) %in% hightlyCorrelated]
+  return(data)
+}
+
+
+dataCleaning <- function(data) {
+  # Remove unnecessary field
+  data <- data[, !names(data)
+               %in% c("prefix", "id", "spkid", "full_name", "pdes",
+                      "name", "orbit_id", "equinox", "neo", "class")]
+  
+  # Remove NA data
+  data <- na.omit(data)
+  
+  # Categorical data
+  data <- transform(
+    data,
+    pha = as.factor(pha)
+  )
+  
+  return(data)
+}
