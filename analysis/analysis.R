cd5fa5274d0a644eaeb68d43ea2c8991d8c49bac
Aman Sinha <aksinha.101010@gmail.com>
2021-09-23 16:00:05 +0000

    initial analysis

--- analysis/analysis.R
+++ analysis/analysis.R
@@ -1,1 +1,45 @@
 # Analysis
+
+data <- read.csv("dataset/dataset.csv", nrows = 5000)
+
+# Dimesion of dataset
+dim(data)
+
+# Structure of dataset
+str(data)
+
+# Removing prefix column as it has only NA values
+data$prefix = NULL
+
+data <- transform(
+  data,
+  neo = as.factor(neo),
+  pha = as.factor(pha),
+  orbit_id = as.factor(orbit_id),
+  equinox = as.factor(equinox),
+  class = as.factor(class)
+)
+
+# NA rows
+data[which(is.na(data)), 1:10]
+
+# Removing rows with NAs
+data <- na.omit(data)
+
+
+# Summary of data
+summary(data)
+
+# Correlation Matrix
+correlationMatrix <- cor(Filter(is.numeric, data))
+correlationMatrix
+
+
+# Plot asteroids diameter distribution
+ggplot(data, aes(x = diameter)) + geom_histogram()
+
+# Plot near earth objects distribution
+ggplot(data, aes(x = neo, fill = neo)) + geom_bar()
+
+# Plot potentially hazardous asteroids distribution
+ggplot(data, aes(x = pha, fill = pha)) + geom_bar()
