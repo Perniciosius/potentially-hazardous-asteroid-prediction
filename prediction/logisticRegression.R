d14d50b11b02392ecf33f2836b45d7c66fd2dfd7
Jay Gupta <jaygupta.id@gmail.com>
2021-11-21 19:20:01 +0530

    Update logisticRegression.R

--- prediction/logisticRegression.R
+++ prediction/logisticRegression.R
@@ -6,17 +6,16 @@ pacman::p_load(dplyr)
 source("utils/helpers.R")
 
 #load the dataset
-data <- read.csv("dataset/dataset.csv",nrows = 400000)
+data <- read.csv(file = "dataset/dataset.csv")
 
 #dataset cleaning
 data <- dataCleaning(data)
 
 #selecting relevent features from the dataset
 data <- featureSelection(data)
-data$pha
-data$pha <- as.integer(data$pha)
-data <- mutate(data, pha=pha-1)
-data$pha
+data$pha <- ifelse(data$pha=='N',0,1)
+#data$pha <- factor(data$pha)
+
 # Split dataset
 sample = sample.split(data$pha, SplitRatio = 0.75)
 train = subset(data, sample == TRUE)
@@ -25,9 +24,8 @@ test = subset(data, sample == FALSE)
 #create model
 model <- glm(formula=pha ~ ., data = train)
 model
-summary(model)
 
 # Predict test set
-pred <- predict(model, test)
+pred <- predict(model, test, type="response")
 pred <- ifelse(pred >0.5, 1, 0)
 confusionMatrix(table(test$pha, pred))
